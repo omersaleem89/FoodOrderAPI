7cb37eecd9c3b37009a87e64db59dd02a7c3a401
omersaleem89 <omersaleem96@gmail.com>
2020-01-30 01:29:14 +0500

    30-01-2020 1:30 am

--- FoodOrderAPI/Areas/Admin/Controllers/CategoryController.cs
+++ FoodOrderAPI/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using FoodOrderAPI.Models;
+using FoodOrderAPI.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,15 +13,42 @@ namespace FoodOrderAPI.Areas.Admin.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Admin")]
     public class CategoryController : ControllerBase
     {
-        [Authorize(Roles ="Admin")]
-        [HttpGet]
-        public IActionResult GetData()
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
         {
-            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
-            var claims = claimsIdentity.FindFirst(ClaimTypes.Email).Value;
-            return new JsonResult("OK");
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+           return new JsonResult(new CategoryViewModel(_db).GetAll());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return new JsonResult(new CategoryViewModel(_db).Get(id));
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromForm] Category category)
+        {
+            return new JsonResult(new CategoryViewModel(_db).Insert(category));
+        }
+        [HttpPut]
+        public IActionResult Update([FromForm] Category category)
+        {
+            return new JsonResult(new CategoryViewModel(_db).Update(category));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return new JsonResult(new CategoryViewModel(_db).Delete(id));
         }
     }
 }
---
+++ FoodOrderAPI/Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodOrderAPI.Models.ViewModels
+{
+    public class CategoryViewModel
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryViewModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            var res = _db.Category.ToList();
+            return res;
+        }
+
+        public Category Get(int id)
+        {
+            var res = _db.Category.FirstOrDefault(x => x.Id == id);
+            return res;
+        }
+
+        public DbResponse Delete(int id)
+        {
+            try
+            {
+                var category = _db.Category.FirstOrDefault(x => x.Id == id);
+                if (category != null)
+                {
+                    _db.Entry(category).State = EntityState.Deleted;
+                    _db.SaveChanges();
+                    return new DbResponse()
+                    {
+                        Result = true,
+                        ExceptionMessage = "Category Deleted"
+                    };
+                }
+                else
+                {
+                    return new DbResponse()
+                    {
+                        Result = false,
+                        ExceptionMessage = "Category does not Exists"
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new DbResponse()
+                {
+                    Result = false,
+                    ExceptionMessage = ex.Message.ToString()
+                };
+            }
+        }
+
+        public DbResponse Insert(Category category)
+        {
+            try
+            {
+                if (_db.Category.FirstOrDefault(x => x.Name == category.Name) == null)
+                {
+                    _db.Category.Add(category);
+                    _db.SaveChanges();
+                    return new DbResponse()
+                    {
+                        Result = true,
+                        ExceptionMessage = "Category Added"
+                    };
+                }
+                else
+                {
+                    return new DbResponse()
+                    {
+                        Result = false,
+                        ExceptionMessage = "Category Already Exists"
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new DbResponse()
+                {
+                    Result = false,
+                    ExceptionMessage = ex.Message.ToString()
+                };
+            }
+
+        }
+
+        public DbResponse Update(Category category)
+        {
+            try
+            {
+                var categoryObj = _db.Category.FirstOrDefault(x => x.Id == category.Id);
+                if (categoryObj != null)
+                {
+                    categoryObj = category;
+                    _db.SaveChanges();
+                    return new DbResponse()
+                    {
+                        Result = true,
+                        ExceptionMessage = "Category Updated"
+                    };
+                }
+                else
+                {
+                    return new DbResponse()
+                    {
+                        Result = false,
+                        ExceptionMessage = "Category does not Exists"
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new DbResponse()
+                {
+                    Result = false,
+                    ExceptionMessage = ex.Message.ToString()
+                };
+            }
+
+        }
+    }
+}
