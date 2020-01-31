65f5ed6355b7136f5bfda706226d3793673cf77e
Muhammad Umer Saleem <omersaleem96@hotmail.com>
2020-01-31 12:10:54 +0500

    31-01-2020

--- FoodOrderAPI/Areas/Account/Controllers/LoginController.cs
+++ FoodOrderAPI/Areas/Account/Controllers/LoginController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FoodOrderAPI.Models;
-using FoodOrderAPI.Models.ViewModels;
+﻿using FoodOrderAPI.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
--- FoodOrderAPI/Models/ViewModels/LoginViewModel.cs
+++ FoodOrderAPI/Models/ViewModels/LoginViewModel.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace FoodOrderAPI.Models.ViewModels
 {
